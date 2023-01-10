bad33f9876baed03c639475cf2b74914b865f3f3
andrew <andrewliu278@gmail.com>
2023-01-10 15:50:26 -0500

    all problem types

--- public/game.js
+++ public/game.js
@@ -3,51 +3,62 @@
 // const socket = io('http://localhost:3000', { transports: ['websocket', 'polling', 'flashsocket'] });
 
 class Game {
-    constructor(problems, time=120){
-        this.amt_time = time;
-        this.time = this.amt_time;
-        this.endtime = 10;
-        this.start_time = new Date().getTime();
-        this.problems = problems
-        this.idx = 0
-    }
+  constructor(problems, time = 120) {
+    this.amt_time = time;
+    this.time = this.amt_time;
+    this.endtime = 10;
+    this.start_time = new Date().getTime();
+    this.problems = problems;
+    this.idx = 0;
+  }
 }
 
-Game.prototype.generate_problem = function(){
-    let problem = this.problems[this.idx]
-    this.ans = problem[2]
-    console.log(problem)
-    document.getElementById("num1").innerHTML = problem[0];
-    document.getElementById("num2").innerHTML = problem[1];
-    // document.getElementById("score").innerHTML = "Score: " + String(this.score);
-}
+Game.prototype.generate_problem = function () {
+  let problem = this.problems[this.idx];
+  this.ans = problem[2];
+  console.log(problem);
+  console.log(problem[3]);
+  document.getElementById("num1").innerHTML = problem[0];
+  document.getElementById("num2").innerHTML = problem[1];
+  let op = document.getElementById("op");
+  if (problem[3] === 0) {
+    op.innerHTML = "&plus;";
+  } else if (problem[3] === 1) {
+    op.innerHTML = "&minus;";
+  } else if (problem[3] === 2) {
+    op.innerHTML = "&times;";
+  } else {
+    op.innerHTML = "&divide;";
+  }
+  // document.getElementById("score").innerHTML = "Score: " + String(this.score);
+};
 
-Game.prototype.check_answer = function(){
-    let input = document.getElementById("user-input").value
-    if (input == this.ans){
-        // this.score+=1;
-        console.log("updating score now")
-        console.log(this.idx)
-        this.idx+=1;
-        socket.emit('update-score', roomName)
-        document.getElementById("user-input").value = ''
-        this.generate_problem()
-        // document.getElementById("check").style.visibility = "hidden"
-    }
-}
+Game.prototype.check_answer = function () {
+  let input = document.getElementById("user-input").value;
+  if (input == this.ans) {
+    // this.score+=1;
+    console.log("updating score now");
+    console.log(this.idx);
+    this.idx += 1;
+    socket.emit("update-score", roomName);
+    document.getElementById("user-input").value = "";
+    this.generate_problem();
+    // document.getElementById("check").style.visibility = "hidden"
+  }
+};
 
 // let timerInterval = null;
-Game.prototype.timer = function(){
-    const now = new Date().getTime()
-    this.time = this.amt_time+Math.round((this.start_time-now)/1000)
-    document.getElementById("timer").innerHTML = "Time: " + String(this.time);
-    // if (this.time==15){
-    //     timerIntervalFast = setInterval(() => this.timerfast(), 10)
-    // }
-    if (this.time == 0){
-        this.end()
-    }
-}
+Game.prototype.timer = function () {
+  const now = new Date().getTime();
+  this.time = this.amt_time + Math.round((this.start_time - now) / 1000);
+  document.getElementById("timer").innerHTML = "Time: " + String(this.time);
+  // if (this.time==15){
+  //     timerIntervalFast = setInterval(() => this.timerfast(), 10)
+  // }
+  if (this.time == 0) {
+    this.end();
+  }
+};
 
 // let timerIntervalFast=null;
 // Game.prototype.timerfast = function(){
@@ -64,28 +75,30 @@ Game.prototype.timer = function(){
 //     }
 // }
 
-Game.prototype.main = function(){
-    document.getElementById("timer").innerHTML = "Time: " + String(this.time);
-    socket.emit('update-score', roomName)
-    this.generate_problem()
-    timerInterval = setInterval(() => this.timer(), 1000)
-}
+Game.prototype.main = function () {
+  document.getElementById("timer").innerHTML = "Time: " + String(this.time);
+  socket.emit("update-score", roomName);
+  this.generate_problem();
+  timerInterval = setInterval(() => this.timer(), 1000);
+};
 
-Game.prototype.endtimer = function(){
-    this.endtime -= 1;
-    if (this.endtime < 0) this.endtime = 0;
-    document.getElementById("final-countdown").innerText = "Redirecting to room in " + String(this.endtime)
-}
+Game.prototype.endtimer = function () {
+  this.endtime -= 1;
+  if (this.endtime < 0) this.endtime = 0;
+  document.getElementById("final-countdown").innerText =
+    "Redirecting to room in " + String(this.endtime);
+};
 
-Game.prototype.end = function(){
-    document.getElementById("game-screen").style.visibility = "hidden"
-    document.getElementById("end-screen").style.visibility = "visible"
-    socket.emit('render-final-ranks', roomName)
+Game.prototype.end = function () {
+  document.getElementById("game-screen").style.visibility = "hidden";
+  document.getElementById("end-screen").style.visibility = "visible";
+  socket.emit("render-final-ranks", roomName);
 
-    document.getElementById("final-countdown").innerText = "Redirecting to lobby in " + String(this.endtime)
-    endtimerInterval = setInterval(() => this.endtimer(), 1000)
-    setTimeout(function () {
-        socket.emit('end-game', roomName)
-        window.location.href = '/room/'+roomName;
-    }, 15000)
-}
+  document.getElementById("final-countdown").innerText =
+    "Redirecting to lobby in " + String(this.endtime);
+  endtimerInterval = setInterval(() => this.endtimer(), 1000);
+  setTimeout(function () {
+    socket.emit("end-game", roomName);
+    window.location.href = "/room/" + roomName;
+  }, 15000);
+};
